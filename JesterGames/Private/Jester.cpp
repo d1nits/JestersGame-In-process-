09f67a1c0a69c97d34199042b530eeb3ef84a9fe
d1nits <wweemoon6@gmail.com>
2024-07-22 19:49:20 +0200

    Update Jester.cpp

--- JesterGames/Private/Jester.cpp
+++ JesterGames/Private/Jester.cpp
@@ -6,7 +6,6 @@
 // Sets default values
 AJester::AJester()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -18,14 +17,12 @@ void AJester::BeginPlay()
 	
 }
 
-// Called every frame
 void AJester::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AJester::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
